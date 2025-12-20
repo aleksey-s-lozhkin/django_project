9fa2998f0ff3ba4e7e740cdc7776a7f70affda0b
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2025-12-20 07:00:35 +0300

    Added catalog app

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.urls import reverse
 
 
 def home(request):
     return render(request, 'home.html')
 
+
 def contacts(request):
+    if request.method == 'POST':
+        # Получаем данные из формы
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        messages.success(request, 'Ваше сообщение успешно отправлено! Мы свяжемся с вами в ближайшее время.')
+
+        return redirect('/contacts/')
+
     return render(request, 'contacts.html')
