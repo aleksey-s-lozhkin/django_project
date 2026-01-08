c27d1e5a587a2ec7602a2befae7a7203c884b7bf
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2026-01-08 14:51:07 +0300

    Setup admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+@admin.register(Product)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+
+@admin.register(Category)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_filter = ('name',)
+    search_fields = ('name',)
