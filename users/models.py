ebf02ebe66451e5e134db6d1cdefc8eeea0082b9
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2026-01-29 21:18:24 +0300

    Fixed mistakes

--- users/models.py
+++ users/models.py
@@ -11,3 +11,11 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['email']
+
+    def __str__(self):
+        return self.email
