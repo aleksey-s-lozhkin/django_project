8c9f9d4668ab73ab08584681f441d5c1c84aa95b
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2026-02-07 09:33:40 +0300

    Realize moderator group

--- catalog/models.py
+++ catalog/models.py
@@ -30,6 +30,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/%Y/%m/', verbose_name='Изображение', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
+    is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
 
@@ -40,6 +41,9 @@ class Product(models.Model):
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
         ordering = ['name']
+        permissions = [
+            ('can_unpublish_product', 'Can unpublish product'),
+        ]
 
 
 class Contact(models.Model):
