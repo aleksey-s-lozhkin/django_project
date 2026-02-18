15e8ebe24aec2f43e80eee14256a9e115b990ce7
Aleksey Lozhkin <aleksey.s.lozhkin@gmail.com>
2026-02-18 20:47:12 +0300

    First commit

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,13 @@ EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+
+CACHE_ENABLE = True
+
+if CACHE_ENABLE:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://localhost:6379',
+        }
+    }
